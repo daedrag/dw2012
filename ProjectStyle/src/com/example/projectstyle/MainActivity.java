c05743f659c78b89a8000fb11cf4325e613df356
daedrag <daoducduy0511@gmail.com>
2012-09-23 04:42:02 +0800

    new target - overlay camera preview

--- ProjectStyle/src/com/example/projectstyle/MainActivity.java
+++ ProjectStyle/src/com/example/projectstyle/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends Activity implements OnClickListener{
         setContentView(R.layout.activity_main);
        
         Button btnCamera = (Button) findViewById(R.id.buttonCamera);
+        Button btnCameraForOverlay = (Button) findViewById(R.id.buttonCameraOverlay);
         btnCamera.setOnClickListener(this);
+        btnCameraForOverlay.setOnClickListener(this);
                
     }
 
@@ -27,6 +29,10 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.buttonCamera:
 			Intent cameraIntent = new Intent(MainActivity.this, CameraActivity.class);
 			startActivity(cameraIntent);
+			break;
+		case R.id.buttonCameraOverlay:
+			Intent cameraIntent2 = new Intent(MainActivity.this, CameraActivityForOverlay.class);
+			startActivity(cameraIntent2);
 			break;
 		default:
 			break;
